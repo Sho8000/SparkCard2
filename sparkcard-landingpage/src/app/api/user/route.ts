fdae11392800738f7ce7a1f58b7f9657ddfd39b7
Sho8000 <yoshikichi8000@gmail.com>
2025-03-05 20:21:04 -0800

    add experimental

--- sparkcard-landingpage/src/app/api/user/route.ts
+++ sparkcard-landingpage/src/app/api/user/route.ts
@@ -1,4 +1,4 @@
-import prisma from "@/app/lib/db";
+import {prisma} from "@/app/lib/db";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
--- sparkcard-landingpage/src/app/lib/db.ts
+++ sparkcard-landingpage/src/app/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+/* import { PrismaClient } from "@prisma/client";
 
 const prisma  = new PrismaClient();
 
@@ -6,4 +6,12 @@ prisma.$connect()
     .then(()=>{console.log("✅ connected to db")} )
     .catch((err:unknown) => {console.log('❌ failed to connect to db', err)})
 
-export default prisma
+export default prisma */
+
+import { PrismaClient } from '@prisma/client';
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (true) globalForPrisma.prisma = prisma;
