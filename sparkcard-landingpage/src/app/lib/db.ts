175fdc1937cec92f86b39a789a219ad0f712f394
Sho8000 <yoshikichi8000@gmail.com>
2025-03-16 16:20:04 -0700

    changed lib db.ts

--- sparkcard-landingpage/src/app/lib/db.ts
+++ sparkcard-landingpage/src/app/lib/db.ts
@@ -7,7 +7,7 @@ prisma.$connect()
     .catch((err:unknown) => {console.log('❌ failed to connect to db', err)})
 
 export default prisma */
-
+/* 
 import { PrismaClient } from '@prisma/client';
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -15,3 +15,15 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (true) globalForPrisma.prisma = prisma;
+ */
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // Logs for debugging
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
